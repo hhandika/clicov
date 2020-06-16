de895955b1530e2527f6e1eb20563d02fcdedaf0
hhandika <herubiolog@gmail.com>
2020-06-15 21:55:54 -0500

    Remove unnecessary functions'

--- utils/search.py
+++ utils/search.py
@@ -1,33 +1,29 @@
-import pandas as pd, requests, json
+import json
 
+import pandas as pd
+import requests
 
 def clean_user_inputs(queries):
     """
+    Convert user inputs to match the api inputs.
 
-    Args:
-        params ([type]): [description]
     """
     queries = queries.lower()
     queries = queries.replace('_', '-')
     return queries
 
 def search_cases(url):
-    """[summary]
+    """
+    Send queries to api and store json return. 
+    The function will check for request status during the process.
 
     Args:
-        url ([type]): [description]
+        url (string): full api query url.
 
     Returns:
-        [type]: [description]
+        json: results from api.
     """
     res = requests.get(url)
     res.raise_for_status()
     data = res.json()
     return data
-
-def save_results(filename):
-    """
-
-    Args:
-        filename ([type]): [description]
-    """
